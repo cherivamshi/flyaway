10f9398bc16cf93a1003829b84a0d38f73d28115
ravi cheri <ravi.cheri@hexagon.com>
2023-08-09 18:48:16 +0530

    initial commit

---
+++ src/main/java/pom/Booking_page.java
@@ -0,0 +1,13 @@
+package pom;
+
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+
+public class Booking_page {
+	
+	@FindBy(xpath="//a[@href='completepurchase']")
+	public static WebElement bookaflight;
+
+}
